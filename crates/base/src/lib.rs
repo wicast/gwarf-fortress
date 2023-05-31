201a47921c5cda3e1941c6432ec7e69c28ea1b72
wicast <wicastchen@hotmail.com>
2023-05-31 20:22:30 +0800

    feat: init

---
+++ crates/base/src/lib.rs
@@ -0,0 +1,208 @@
+use std::any::Any;
+
+use as_any::{AsAny, Downcast};
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+
+
+type InitFn = fn(state: &mut Box<dyn StateDynObj>) -> ();
+type TickFn = fn(state: &mut BaseState) -> ();
+type RenderFn = fn(state: &mut BaseState) -> Result<(), wgpu::SurfaceError>;
+
+pub trait StateDynObj: AsAny {
+}
+
+pub fn cast_mut<OT: 'static>(state: &mut Box<dyn StateDynObj>) ->Option<&mut OT> {
+    state.as_mut().downcast_mut::<OT>()
+}
+
+pub struct BaseState {
+    pub surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub config: wgpu::SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
+    pub window: Window,
+
+    pub extra_state: Box<dyn StateDynObj>,
+    pub render_fn: RenderFn,
+    pub tick_fn: TickFn,
+}
+
+impl BaseState {
+    // Creating some of the wgpu types requires async code
+    async fn new(
+        window: Window,
+        mut in_state: Box<dyn StateDynObj>,
+        init_fn: InitFn,
+        tick_fn: TickFn,
+        render_fn: RenderFn,
+    ) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        // # Safety
+        //
+        // The surface needs to live as long as the window that created it.
+        // State owns the window so this should be safe.
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    // WebGL doesn't support all of wgpu's features, so if
+                    // we're building for the web we'll have to disable some.
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        init_fn(&mut in_state);
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_fn,
+            tick_fn,
+            extra_state: in_state,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn tick(&mut self) {
+        (self.tick_fn)(self)
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        (self.render_fn)(self)
+    }
+}
+
+pub async fn run(
+    in_state: Box<dyn StateDynObj>,
+    init_fn: InitFn,
+    tick_fn: TickFn,
+    render_fn: RenderFn,
+) {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = BaseState::new(window, in_state, init_fn, tick_fn, render_fn).await;
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                state.tick();
+                match state.render() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if lost
+                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            Event::MainEventsCleared => {
+                // RedrawRequested will only trigger once, unless we manually
+                // request it.
+                state.window().request_redraw();
+            }
+            _ => {}
+        }
+    });
+}
---
+++ crates/samples/examples/blank_window.rs
@@ -0,0 +1,60 @@
+use gf_base::{cast_mut, run, BaseState, StateDynObj};
+
+struct State {
+    i: u32,
+}
+
+impl StateDynObj for State {}
+
+fn render(state: &mut BaseState) -> Result<(), wgpu::SurfaceError> {
+    let output = state.surface.get_current_texture()?;
+    let view = output
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
+
+    let mut encoder = state
+        .device
+        .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+    {
+        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        });
+    }
+
+    // submit will accept anything that implements IntoIter
+    state.queue.submit(std::iter::once(encoder.finish()));
+    output.present();
+    Ok(())
+}
+
+fn main() {
+    pollster::block_on(run(
+        Box::new(State { i: 3222 }),
+        |state| {
+            let mut state = cast_mut::<State>(state).unwrap();
+            state.i = 3213312;
+        },
+        |state| {
+            // let state = cast_mut::<State>(&mut state.extra_state).unwrap();
+            // println!("state: {}", state.i)
+        },
+        render,
+    ))
+}
